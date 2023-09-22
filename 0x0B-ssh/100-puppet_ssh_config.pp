596fee72cd81011f2d8ef503727459190f6d668d
zrdouane <ramirdouane@gmail.com>
2023-09-22 09:38:59 -0700

    client configuration file

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# Puppet to make changes to our configuration file.
+
+$file_content = file('/etc/ssh/ssh_config')
+$config = "${file_content}\
+    IdentityFile ~/.ssh/school
+    PasswordAuthentication no
+"
+file {  'school':
+  ensure  => 'present',
+  path    => '/etc/ssh/ssh_config',
+  content => $config
+}
