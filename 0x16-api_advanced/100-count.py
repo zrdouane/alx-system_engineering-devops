f6ed77ba8ba9e6f775a54f8b19ff16cb1a7f95cf
zrdouane <ramirdouane@gmail.com>
2023-11-07 02:08:04 -0800

    fix is not in line 75

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -72,7 +72,7 @@ def count_words(subreddit, word_list):
 
     if len(lo) != 0:
         for item in lo:
-            if item[1] is not 0:
+            if item[1] != 0:
                 print("{}: {}".format(item[0], item[1]))
     else:
         print("")
